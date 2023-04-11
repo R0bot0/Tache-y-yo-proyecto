f7636e22adceadadca26951c3cd0d8281b69e9b6
R0bot0 <rmaldonadoe@uninorte.edu.co>
2023-04-11 02:54:00 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'lo.que.hizo.tache.en.su.tiempo',
+  appName: 'lo-que-hizo-tache-en-su-tiempo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
