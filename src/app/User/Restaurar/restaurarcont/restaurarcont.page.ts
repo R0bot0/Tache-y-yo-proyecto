fdf9d0f87784304444ad2637390150dd4d46c5ec
R0bot0 <rmaldonadoe@uninorte.edu.co>
2023-05-16 15:24:35 -0500

    Restaurar contraseña1 empezado

--- src/app/User/Restaurar/restaurarcont/restaurarcont.page.ts
+++ src/app/User/Restaurar/restaurarcont/restaurarcont.page.ts
@@ -2,19 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-restaurarcont',
   templateUrl: './restaurarcont.page.html',
   styleUrls: ['./restaurarcont.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink, RouterLinkActive ]
 })
 export class RestaurarcontPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  mandarnotif() {
+    const body = {
+    };
   }
 
 }
