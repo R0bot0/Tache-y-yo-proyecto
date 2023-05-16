726d14991b105a2e8eea57d192783d03c0fee817
R0bot0 <rmaldonadoe@uninorte.edu.co>
2023-05-16 14:40:52 -0500

    Estoy empezando a hacer el restaurar contraseña

--- src/app/User/inicio/inicio.page.ts
+++ src/app/User/inicio/inicio.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, RouterLink, RouterLinkActive, CommonModule, FormsModule]
 })
 export class InicioPage implements OnInit {
   currentday: String = new Date().toLocaleString('es-ES', {weekday: 'long'});
