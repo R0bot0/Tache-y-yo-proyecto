c90600ddaec1c9376cb93141a17c08d937e82cc6
R0bot0 <rmaldonadoe@uninorte.edu.co>
2023-05-16 14:33:10 -0500

    Hice el cambio de contraseña

--- src/app/User/cambiarcont/cambiarcont.page.ts
+++ src/app/User/cambiarcont/cambiarcont.page.ts
@@ -2,19 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-cambiarcont',
   templateUrl: './cambiarcont.page.html',
   styleUrls: ['./cambiarcont.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, RouterLink, RouterLinkActive, CommonModule, FormsModule]
 })
 export class CambiarcontPage implements OnInit {
-
-  constructor() { }
+  oldpassword!: string;
+  newpassword!: string;
+  repeatpassword!: string;
+  constructor() {}
 
   ngOnInit() {
+  }
+  
+  neuvacont() {
+    const body = {
+      oldpassword: this.oldpassword,
+      newpassword: this.newpassword,
+      repeatpassword: this.repeatpassword
+    };
+    
+    console.log(body)
+    // this.http.post(`${environment.apiUrl}/auth/login`, body).subscribe((response) => {
+    //   if (response) {
+    //     // User exists, navigate to home page
+    //     this.router.navigate(['folder/Documentos de interes']);
+    //   } else {
+    //     // Show error message
+    //     console.log("error del servidor");
+    //   }
+    // });
   }
 
 }
